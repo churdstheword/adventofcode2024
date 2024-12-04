3138182d67ac9959826b02c8e5d0d6cc990b27c4
Richard Lovern <richard.lovern@starcitygames.com>
2024-12-04 17:48:13 -0500

    More updates to day 01

--- src/day01/solution.py
+++ src/day01/solution.py
@@ -3,7 +3,6 @@
 
 import os
 
-
 def parseInput(filename: str) -> list[list[int]]:
     lists = [[],[]]
     dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,31 +13,25 @@ def parseInput(filename: str) -> list[list[int]]:
             lists[1].append(int(columns[1]))
     return lists
 
+def partOne(a: list[int], b: list[int]) -> int:
+    a.sort()
+    b.sort()
+    dist = 0
+    for i in range(len(a)):
+        dist += abs(a[i] - b[i])
+    return dist
 
-def partOne() -> int:
-    lists = parseInput('input.txt')
-    lists[0].sort()
-    lists[1].sort()
-    distance = 0
-    for i in range(len(lists[0])):
-        distance += abs(lists[0][i] - lists[1][i])
-        
-    return distance
-
-def partTwo() -> int:
-    lists = parseInput('input.txt')
+def partTwo(a: list[int], b: list[int]) -> int:
     score = 0
-    for i in range(len(lists[0])):
-        value = lists[0][i]
-        freq = lists[1].count(value)
-        score += value * freq
-
+    for i in range(len(a)):
+        val = a[i]
+        freq = b.count(val)
+        score += val * freq
     return score
 
-
 if __name__ == '__main__':
-    solution1 = partOne()
+    lists = parseInput('input.txt')    
+    solution1 = partOne(lists[0], lists[1])
     print(f'The solution to part one: {str(solution1)}')
-    solution2 = partTwo()
+    solution2 = partTwo(lists[0], lists[1])
     print(f'The solution to part two: {str(solution2)}')
-    
